0c5d546c802c55b64845c9841d1751f25b3903a4
mathkimchi <kingalvinjkim@gmail.com>
2025-01-12 16:51:45 -0500

    add harpoon shooting

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+18 into dev:
+
+I will try to log my changes in here along with times.
+I will work on harpoon shooting.
+
+43 min:
+
+I implemented basic harpoon shooting.
--- player.gd
+++ player.gd
@@ -6,21 +6,40 @@ var CHARGE_RATE = 1.0
 var harpoon_charge = 0.0
 var is_harpoon_shot = false
 
+var harpoon_position: Vector2
+var harpoon_velocity: Vector2
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	if !is_harpoon_shot:
+	if is_harpoon_shot:
+		harpoon_physics_process(delta)
+	else:
 		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 			harpoon_charge += CHARGE_RATE * delta
 
 	move_and_slide()
 	
-func shoot_harpoon() -> void:
+func harpoon_physics_process(delta: float) -> void:
+	harpoon_velocity += get_gravity() * delta
+	harpoon_position += harpoon_velocity * delta
+	
+	$Harpoon.global_position=harpoon_position
+	
+func calc_initial_harpoon_speed() -> float:
+	return clampf(1000*harpoon_charge**2, 500, 4000)
+	
+# Uses event to calculate direction from player to mouse
+func shoot_harpoon(event) -> void:
+	print("Shot with charge: ", harpoon_charge, " and speed: ", calc_initial_harpoon_speed())
+	
+	harpoon_position = self.global_position
+	var harpoon_initial_direction = self.get_global_transform_with_canvas().get_origin().direction_to(event.position)
+	harpoon_velocity = calc_initial_harpoon_speed() * harpoon_initial_direction
+	
 	# reset the charge
-	print("Shot with charge: ", harpoon_charge)
 	harpoon_charge = 0.0
 	is_harpoon_shot = true
 
@@ -32,4 +51,4 @@ func _input(event: InputEvent) -> void:
 				pass # TODO
 			else:
 				# was not shot, so shoot
-				shoot_harpoon()
+				shoot_harpoon(event)
